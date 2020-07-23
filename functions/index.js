8e6a6e53b7b9813f3c7804955ed797ea79a42d59
Gongsta <gong.steven@hotmail.com>
2020-07-22 21:32:46 -0400

    removed user-infos fuctions

--- functions/index.js
+++ functions/index.js
@@ -86,37 +86,6 @@ exports.dialogflowWebhook = functions.https.onRequest(async (request, response) 
 });
 
 
-//Firestore trigger, Creating user information in user-infos
-
-exports.createUser = functions.firestore
-  .document('users/{userId}')
-  .onCreate((snap, context) => {
-    // Get an object representing the document
-    // e.g. {'name': 'Marie', 'age': 66}
-    const newValue = snap.data();
-
-    db.collection('user-infos').doc(context.params.userId).set(newValue)
-
-  });
-
-//Firestore trigger, updating user information in user-infos
-exports.updateUser = functions.firestore
-  .document('users/{userId}')
-  .onUpdate((change, context) => {
-    // Get an object representing the document
-    // e.g. {'name': 'Marie', 'age': 66}
-    const newValue = change.after.data();
-
-    // ...or the previous value before this update
-    //const previousValue = change.before.data();
-
-    db.collection('user-infos').doc(context.params.userId).set(newValue, {
-      merge: true
-    })
-
-
-  });
-
 
 exports.createOrgId = functions.firestore
   .document("organizations/{orgId}")
